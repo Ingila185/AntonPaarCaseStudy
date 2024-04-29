384cb4779536d8f84c98b7503f27d8d44178a41f
ingila185 <ingila185@gmail.com>
2024-04-29 12:56:29 +0500

    reduced delay of API fetch

--- cypress/e2e/FirstPage.cy.ts
+++ cypress/e2e/FirstPage.cy.ts
@@ -1,7 +1,7 @@
 describe('First Page loads and data is fetched and plotted into graph', () => {
   it('Loads the server side page and fetches data', () => {
     cy.visit('http://localhost:3000/');
-    cy.wait(20000) //wait 20 seconds for the Recharts library to plot values
+    cy.wait(10000) //wait 10 seconds for the Recharts library to plot values
     cy.get('.recharts-surface').should('be.visible'); //Recharts should appear 
     cy.screenshot();
 
@@ -11,7 +11,7 @@ describe('First Page loads and data is fetched and plotted into graph', () => {
 describe('User is able to POST new readings', () => {
   it('User successfully posts new reading', () => {
     cy.visit('http://localhost:3000/');
-    cy.wait(20000) //wait 20 seconds for the Recharts library to plot values
+    cy.wait(10000) //wait 10 seconds for the Recharts library to plot values
     cy.get('#measuredAt').type(new Date().toISOString().slice(0, 10));
     cy.get('#density').type("12.54");
     cy.get('#temperature').type("20.25");
